0ceb5bd7e0c227be0c71df50f77deb9e09ef9897
sumonbarai <sumonbarai78@gmail.com>
2022-06-30 20:31:15 +0600

    post api create

--- index.js
+++ index.js
@@ -7,6 +7,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3pzd9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const billingCollection = client.db("powerHack").collection("billing-list");
+    //  post api
+    app.post("/add-billing", async (req, res) => {
+      const insertedBill = req.body;
+      const result = await billingCollection.insertOne(insertedBill);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("server is running my project");
 });
