54f08e9ee420752770d49fbc9dd510057a0c8482
sumonbarai <sumonbarai78@gmail.com>
2022-07-01 11:24:52 +0600

    update work

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3pzd9.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -19,7 +19,6 @@ async function run() {
     await client.connect();
     const billingCollection = client.db("powerHack").collection("billing-list");
     // get api
-
     app.get("/billing-list", async (req, res) => {
       const query = {};
       const cursor = billingCollection.find(query);
@@ -30,6 +29,29 @@ async function run() {
     app.post("/add-billing", async (req, res) => {
       const insertedBill = req.body;
       const result = await billingCollection.insertOne(insertedBill);
+      res.send(result);
+    });
+    // update api
+    app.put("/update-billing/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedBill = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: updatedBill,
+      };
+      const result = await billingCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+    // delete api
+    app.delete("/delete-billing/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await billingCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
